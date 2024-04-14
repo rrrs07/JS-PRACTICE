7f5253631584d169c95be36631b45249e546505e
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-14 23:16:51 +0530

    cal the sum of only +ve nos.

---
+++ practice questions/rem_1st&last_char.js
@@ -0,0 +1,6 @@
+function removeChar(str){
+    const lengthOfString = str.length;
+     
+     return str.substr(1, lengthOfString - 2);
+     
+   };
---
+++ practice questions/vowel.js
@@ -0,0 +1,26 @@
+function getCount(str) {
+    let vowelC = 0;
+    
+    const arr = str.split("");
+    
+    for ( let i = 0 ; i<arr.length; i++){
+      switch(arr[i] ){
+          case 'a':
+           vowelC++;
+           break;
+          case 'e':
+           vowelC++;
+           break;
+          case 'i':
+           vowelC++;
+           break;
+          case 'o':
+           vowelC++;
+           break;
+          case 'u':
+           vowelC++;
+           break;
+  }
+    }
+    return vowelC;
+  }

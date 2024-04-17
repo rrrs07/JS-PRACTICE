f05a720052df5456af2ba68f55cfbc03c164a249
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-17 23:59:36 +0530

    updated

--- concepts/callBack_functions.js
+++ concepts/callBack_functions.js
@@ -3,11 +3,14 @@ function myCallback(someNumber) {
 }
 
 function mainFunction(randomNumber,shouldCall, callback){
-    let result = randomNumber;
+    let result = randomNumber; // in this ex result === 5
 
+    //in this ex , shouldCall is 'true', so we do reach the callback
     if(shouldCall){
+        // in this ex,'callback' represents 'myCallback' from above
         result = callback(randomNumber);
     }
+    //since 'result' was re-assigned by the callback function,returns 10
     return result;
 
 }

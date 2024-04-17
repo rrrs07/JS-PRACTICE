8dd0320dd104bc923b29b8e8dc81fecd63c78855
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-04-18 01:20:34 +0530

    array methods

--- concepts/array.js
+++ concepts/array.js
@@ -1,3 +1,9 @@
 const strArr = ['a', 'b', 'c', 'd', 'e'];
 strArr.join(""); 
 // console.log(strArr); 
+
+const arr = ["hello", "world", "how", "are", "you"];
+arr.map(itemFromArr => itemFromArr[0]);
+// getting the first element ----- ["h", "w", "h", "a", "y"]
+  
+
